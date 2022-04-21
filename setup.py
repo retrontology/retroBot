fd044dacb7be831886be7fc20608b21606c8b7a2
Retrontology <retrontology@hotmail.com>
2022-04-21 18:39:41 -0500

    remove delays

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setuptools.setup(
     name="retroBot",
-    version="0.1.12",
+    version="0.1.13",
     author="retrontology",
     author_email="retrontology@hotmail.com",
     description="A Twitch IRC bot integrating twitchAPI and irc",
